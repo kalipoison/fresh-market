6709a9ba834d9473d37409a675aee4ace437dfba
kalipoison <1174616074@qq.com>
2022-04-19 00:14:58 +0800

    media data setting

--- 生鲜超市/MxShop/MxShop/settings.py
+++ 生鲜超市/MxShop/MxShop/settings.py
@@ -129,3 +129,9 @@ USE_TZ = False   #默认是Ture，时间是utc时间，由于我们要用本地�
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = "/media/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- 生鲜超市/MxShop/MxShop/urls.py
+++ 生鲜超市/MxShop/MxShop/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 # from django.contrib import admin
+from MxShop.settings import MEDIA_ROOT
+from django.views.static import serve
 import xadmin
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
 ]
